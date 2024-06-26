f59426b7fc8c13d6bfdc94c4290e37853a6b4ccc
weysi <uve.eng@outlook.com>
2024-06-26 10:05:28 +0200

    edited

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<main className='not-found'>
+			<h1> Not Found </h1>
+			<p>Unfortunately, we could not find the request page or resource</p>
+		</main>
+	);
+}
