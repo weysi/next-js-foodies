e6c1db72d4e1597c1a7a0f81fa445552d3b13c2f
weysi <uve.eng@outlook.com>
2024-07-22 12:25:55 +0200

    edited

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,6 +2,12 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+
+export const metadata = {
+	title: 'All Meals',
+	description: 'Browse the delicious meals shared by our vibrant community',
+};
+
 export default async function MealsPage() {
 	const meals = await getMeals();
 
--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -41,7 +41,7 @@ export default function ImagePicker({ label, name }) {
 					className={classes.input}
 					type='file'
 					id='image'
-					accept='image/png, image/jpeg'
+					accept='image/png, image/jpeg, image/jpg'
 					name={name}
 					ref={imageInput}
 					onChange={handleImageChange}
